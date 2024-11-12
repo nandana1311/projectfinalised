411f3fcebb1ff2a8fd7a17f8e8438a8cece85137
nandana1311 <164927105+nandana1311@users.noreply.github.com>
2024-11-12 23:47:43 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+ 
+  const form = document.querySelector('form');
+  
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    alert("Form submitted!");
+  });
+});
